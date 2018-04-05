98e4c8f6c3047a10898fed6eed7298f25ece7e08
Dinesh <dineshatolia9@gmail.com>
2018-04-05 13:52:19 +0530

    Removed Bearer Before Token

--- api/policies/auth.policy.js
+++ api/policies/auth.policy.js
@@ -6,15 +6,15 @@ module.exports = (req, res, next) => {
   if (req.header('Authorization')) {
     const parts = req.header('Authorization').split(' ');
 
-    if (parts.length === 2) {
-      const scheme = parts[0];
-      const credentials = parts[1];
-
-      if (/^Bearer$/.test(scheme)) {
+    if (parts.length === 1) {
+      const credentials = parts[0];
+      /*const credentials = parts[1];*/
+      token = credentials;
+      /*if (/^Bearer$/.test(scheme)) {
         token = credentials;
       } else {
         return res.status(401).json({ msg: 'Format for Authorization: Bearer [token]' });
-      }
+      }*/
     } else {
       return res.status(401).json({ msg: 'Format for Authorization: Bearer [token]' });
     }
